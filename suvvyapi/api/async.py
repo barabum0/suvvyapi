3e1dfc4f84fb5d169cf61c12353b988dbc19181f
sushka <r.m.poltorabatko@gmail.com>
2024-12-29 17:50:04 +0300

    send_message

--- suvvyapi/api/async.py
+++ suvvyapi/api/async.py
@@ -1,8 +1,17 @@
+import string
+from random import choices
+from typing import BinaryIO
+
 import httpx
+
+from suvvyapi.types.attachment import Attachment, input_to_attachments
+from suvvyapi.types.enum import SuvvyMessageSender
+from suvvyapi.types.link import Link
 
 
 class AsyncSuvvy(httpx.AsyncClient):
     """Asynchronous client for Suvvy AI API"""
+
     def __init__(
         self,
         token: str,
@@ -11,10 +20,56 @@ class AsyncSuvvy(httpx.AsyncClient):
     ) -> None:
         super().__init__(
             base_url=base_url,
-            headers={
-                "Authorization": f"Bearer {token}"
-            },
+            headers={"Authorization": f"Bearer {token}"},
             timeout=30,
         )
 
-    async def
+    async def send_message(
+        self,
+        chat_id: str,
+        source: str,
+        text: str | None = None,
+        attachments: list[Attachment | str | tuple[str, bytes] | BinaryIO]
+        | None = None,
+        placeholders: dict[str, str] | None = None,
+        link: Link | None = None,
+        client_name: str | None = None,
+        client_phone: str | None = None,
+        message_sender: SuvvyMessageSender = SuvvyMessageSender.CUSTOMER,
+        message_id: str | None = None,
+    ) -> None:
+        """Send message to Suvvy
+
+        :param chat_id: Chat ID
+        :param source: Source
+        :param text: Message text. Required if attachments are not specified.
+        :param attachments: Attachments. Required if text is not specified.
+        :param placeholders: Instruction placeholders
+        :param link: Link
+        :param client_name: Client name
+        :param client_phone: Client phone
+        :param message_sender: Message sender (customer or employee)
+        :param message_id: Message ID. Used to prevent repeated answers. Created randomly if not specified.
+        """
+        message_id = message_id or "".join(
+            choices(string.ascii_letters + string.digits, k=10)
+        )
+        if text is None and not attachments:
+            raise ValueError("Either text or attachments must be specified")
+
+        await self.post(
+            "/api/webhook/custom/message",
+            json={
+                "api_version": 1,
+                "chat_id": chat_id,
+                "source": source,
+                "text": text,
+                "attachments": [a.to_dict() for a in input_to_attachments(attachments)] if attachments else [],
+                "placeholders": placeholders,
+                "link": link,
+                "client_name": client_name,
+                "client_phone": client_phone,
+                "message_sender": message_sender.value,
+                "message_id": message_id,
+            },
+        )
